2c2000a2c8faf8986aaaf19c43cea5e0c89312bc
thill2 <hill341539@gmail.com>
2018-06-15 23:36:04 -0400

    commit new code from other repo

--- README.md
+++ README.md
@@ -1,45 +1,13 @@
-# ScaleUp!
-## Project 2
+# ScaleUp
 
-## Background
-This app is called ScaleUp and it was done as a project for the February 2018 UNC Coding Boot Camp.
-
-## How to get started with the app
-The application is online at https://scale-up20182.herokuapp.com/. You will more than likely have to wait a little while after visiting this url before Heroku spins up the virtual machine which hosts the project.
-
-Please install Git Bash, MySQL, and Node.js if you want to use the app locally. 
-
-After all of these are installed, you can download the GitHub Repo.
-
-In the project's root directory:
-  Install the database from the command line by by entering:
-
-      mysql -u root -p < schema.sql
-      Enter password: <enter password here>
-
-      mysql -u root -p < seeds.sql
-      Enter password: <enter password here>
-
-  Next install the project dependencies by entering:
-
-      npm install
-
-The project is now ready to run!
-
-Start the server by typing in:
-
-    node server.js
-
-Open a browser and navigate to:
-
-  http://localhost:8080/ 
-
-in the address line.
+# CHECK db PASSWORD in the CONFIG FILE!!! Make sure its your password.
 
 
-You can enter utilities for Building 1, Zones 1-3, & Building 2, Zones 1-3
+This is a temporary place to put info that may otherwise get lost
+Inorder to run the app I had to comment out the following in \ScaleUp\node_modules\express\lib\application.js
 
-Navigate over to the results page, and you'll see the percent utilization for each of the zones in each building.  You can click on any of the zones aboe the chart to temporarily remove them from the chart.
+Lines 209-211
+  // if (fns.length === 0) {
+  //   throw new TypeError('app.use() requires a middleware function')
+  // }
 
-## Authors
-This app was built by Jeannie Rose, Walheed Hamza, & Tony Hill.
--- public/js/chart.js
+++ public/js/chart.js
@@ -4,7 +4,8 @@
 // Could be broken into a different js file, maybe app.js
 $(document).ready(function(){
   $.ajax({
-    url: "https://scale-up2018.herokuapp.com/api/data",
+    // url: "http://localhost:8080/api/data",
+    url: "http://" + window.location.hostname + ":8080/api/data",
     method: "GET",
     dataType: 'json',
     success: function(data){
@@ -16,6 +17,8 @@ $(document).ready(function(){
       for(var i in data.Buildings) {
         buildingNames.push(data.Buildings[i].building_name);
         zoneNames.push(data.Zones[i].plant_zones);
+        // multiple utilities in single row, how are we looking to chart this data?
+        // utilityNames.push(data[i].utility1name);
       }
       console.log(buildingNames);
       console.log(zoneNames);
@@ -24,57 +27,35 @@ $(document).ready(function(){
       // These arrays are being generated but not used, will become Chart labels
       
       utilityNames=[
-        data.Utilities["0"].utility1name,
-        data.Utilities["0"].utility2name,
-        data.Utilities["0"].utility3name,
-        data.Utilities["0"].utility4name
+        data.Utilities[0].utility1name,
+        data.Utilities[0].utility2name,
+        data.Utilities[0].utility3name,
+        data.Utilities[0].utility4name
       ]
       console.log(utilityNames);
 
-      utilityPercent1=[
+      utilityPercent=[
         data.Utilities["0"].percent_utilizations1,
         data.Utilities["0"].percent_utilizations2,
         data.Utilities["0"].percent_utilizations3,
         data.Utilities["0"].percent_utilizations4
       ]
-
-      utilityPercent2=[
-        data.Utilities["1"].percent_utilizations1,
-        data.Utilities["1"].percent_utilizations2,
-        data.Utilities["1"].percent_utilizations3,
-        data.Utilities["1"].percent_utilizations4
-      ]
-
-      utilityPercent3=[
-        data.Utilities["2"].percent_utilizations1,
-        data.Utilities["2"].percent_utilizations2,
-        data.Utilities["2"].percent_utilizations3,
-        data.Utilities["2"].percent_utilizations4
-      ]
-
+      console.log(utilityPercent);
 
       var chart1data ={
         labels: utilityNames,
         datasets: [{
-          label: 'Zone 1',
+          label: 'Percent Used first row',
           backgroundColor: 'rgba(60, 80, 235, 0.8)',
-          data: utilityPercent1
-          },{
-            label: 'Zone 2',
-            backgroundColor: 'rgba(40, 100, 235, 0.8)',
-            data: utilityPercent2
-          },{
-              label: 'Zone 3',
-              backgroundColor: 'rgba(20, 120, 235, 0.8)',
-              data: utilityPercent3
+          data: utilityPercent
         }],
       };
 
       // Actual chart creation
       const chartOne = document.getElementById('chartOne');
       Chart.defaults.global.defaultFontFamily = 'lato';
-      Chart.defaults.global.defaultFontSize = 16;
-      Chart.defaults.global.defaultFontColor = 'white';
+      Chart.defaults.global.defaultFontSize = 17;
+      Chart.defaults.global.defaultFontColor = 'black';
       
       var barChartOne = new Chart(chartOne, {
         type: 'bar',
@@ -90,46 +71,37 @@ $(document).ready(function(){
           },
           title:{
             display: true,
-            text: 'This is ' + buildingNames[0] 
+            text: 'This is ' + buildingNames[0] + ' ' + zoneNames[0] 
           }},
       });
 
 
-      utilityPercent4=[
-        data.Utilities["3"].percent_utilizations1,
-        data.Utilities["3"].percent_utilizations2,
-        data.Utilities["3"].percent_utilizations3,
-        data.Utilities["3"].percent_utilizations4
-      ]
 
-      utilityPercent5=[
-        data.Utilities["4"].percent_utilizations1,
-        data.Utilities["4"].percent_utilizations2,
-        data.Utilities["4"].percent_utilizations3,
-        data.Utilities["4"].percent_utilizations4
-      ]
+// chart 2
+utilityNames=[
+  data.Utilities["1"].utility1name,
+  data.Utilities["1"].utility2name,
+  data.Utilities["1"].utility3name,
+  data.Utilities["1"].utility4name
+]
+//console.log(utilityNames);
 
-      utilityPercent6=[
-        data.Utilities["5"].percent_utilizations1,
-        data.Utilities["5"].percent_utilizations2,
-        data.Utilities["5"].percent_utilizations3,
-        data.Utilities["5"].percent_utilizations4
-      ]
+utilityPercent=[
+  data.Utilities["1"].percent_utilizations1,
+  data.Utilities["1"].percent_utilizations2,
+  data.Utilities["1"].percent_utilizations3,
+  data.Utilities["1"].percent_utilizations4
+]
+//console.log(utilityPercent);
+
+
 
 var chart2data ={
   labels: utilityNames,
   datasets: [{
-    label: 'Zone 1',
-    backgroundColor: 'rgba(60, 80, 235, 0.8)',
-    data: utilityPercent4
-    },{
-      label: 'Zone 2',
-      backgroundColor: 'rgba(40, 100, 235, 0.8)',
-      data: utilityPercent5
-    },{
-        label: 'Zone 3',
-        backgroundColor: 'rgba(20, 120, 235, 0.8)',
-        data: utilityPercent6
+    label: 'Percent Used second row',
+    backgroundColor: 'rgba(60, 100, 200, 0.8)',
+    data: utilityPercent
   }],
 };
 
@@ -150,7 +122,7 @@ var barChartTwo = new Chart(chartTwo, {
     },
     title:{
       display: true,
-      text: 'This is ' + buildingNames[4] 
+      text: 'This is ' + buildingNames[1] + ' ' + zoneNames[1] 
     }},
 });
 
